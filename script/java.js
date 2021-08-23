d19ebf2e2f1fbaa9f3a9d58f1e4adb82de1bc553
bobur <officebobur34@gmail.com>
2021-08-23 04:49:12 -0800

    videoslider

--- script/java.js
+++ script/java.js
@@ -1,24 +1,37 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", function(){
-const videos = document.querySelectorAll("#videomp4"),
-    prev = document.querySelector("#prev"),
-    next = document.querySelector("#next"),
-    menubar = document.querySelector(".menu-bar"),
-    bodyContent = document.querySelector(".body-content");
+const tabsParent = document.querySelectorAll("#videomp4"),
+        slider = document.querySelector('#slider');
 
-console.log(videos);
-videos.forEach((item) =>{
-    item.addEventListener("click", function(){
-        item.classList.toggle('active');
-    });
-});
-// prev.addEventListener("click", function(){
-//     const prevFunk = function() {
-//         menubar.classList.toggle("prevactive");
-//         bodyContent.classList.add("bodyshow");
-//     };
-//     prevFunk();
 
-// });
-
-});   
+        function hideTabContent() {
+            tabContent.forEach((item) => {
+                item.style.display = "none";
+            });
+            tabs.forEach((item) => {
+                item.classList.remove("tabheader__item_active");
+            });
+            }
+        
+            function showTabContent(i = 0) {
+            tabContent[i].style.display = "block";
+            tabs[i].classList.add("tabheader__item_active");
+            }
+        
+            hideTabContent();
+            showTabContent();
+        
+            headerParents.addEventListener("click", (event) => {
+            console.log(event);
+            const target = event.target;
+            if (target && target.classList.contains("tabheader__item")) {
+                tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+                });
+            }
+            });
+        });
+        
